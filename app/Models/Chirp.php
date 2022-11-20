bbc20eb54a1eacf74409161a8859c40e250f8cf3
zeapiaji <89508245+zeapiaji@users.noreply.github.com>
2022-11-20 16:29:07 +0700

    first commit

---
+++ app/Models/Chirp.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Chirp extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'message',
+    ];
+
+    /**
+     * Get the user that owns the Chirp
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
